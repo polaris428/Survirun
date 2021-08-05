c40cf745fa841d812e92535190b21248b1608c24
polairs428 <iou1056212@naver.com>
2021-08-05 22:48:11 +0900

    스토리 타이핑 효과로 보여주기

---
+++ app/src/main/java/com/example/survirun/SingUpProfile.java
@@ -0,0 +1,14 @@
+package com.example.survirun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingUpProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up_profile);
+    }
+}
