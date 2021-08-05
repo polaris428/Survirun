9e2f1d6108e2ca81a527e77889899f016cbc1ecd
polairs428 <iou1056212@naver.com>
2021-08-06 00:05:33 +0900

    바텀 네비겨이션바를 통한 화면 전환

--- app/src/main/java/com/example/survirun/Activity/MainActivity.java
+++ app/src/main/java/com/example/survirun/Activity/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.survirun.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.example.survirun.ExerciseFragment;
+import com.example.survirun.FriendFragment;
 import com.example.survirun.R;
+import com.example.survirun.StatisticsFragment;
+import com.example.survirun.UserFragment;
 import com.example.survirun.databinding.ActivityMainBinding;
 import com.example.survirun.databinding.ActivitySingUpBinding;
+import com.google.firebase.storage.internal.Util;
+
+import java.util.function.Function;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -23,6 +35,38 @@ public class MainActivity extends AppCompatActivity {
         binding.meowBottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.common_full_open_on_phone));
         binding.meowBottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.common_google_signin_btn_icon_dark_focused));
         binding.meowBottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.common_google_signin_btn_icon_dark_normal_background));
-        binding.meowBottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.ic_launcher_background));
+        binding.meowBottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.common_google_signin_btn_icon_dark_normal_background));
+        binding.meowBottomNavigation.show(1,true);
+        replace(new UserFragment());
+        binding.meowBottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                switch (model.getId()){
+                    case 1:
+                        replace(new UserFragment());
+                        break;
+                    case 2:
+                        replace(new StatisticsFragment());
+                        break;
+                    case 3:
+                        replace(new ExerciseFragment());
+                        break;
+
+                    case 4:
+                        replace(new FriendFragment());
+                        break;
+
+
+
+                }
+                return null;
+            }
+        });
+
+    }
+    private void replace(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout,fragment);
+        transaction.commit();
     }
 }
