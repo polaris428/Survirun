be8a102a99e6d1ebfd0dfa55ae91314d91331a83
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-08-10 00:49:17 +0900

    나의 목표수정 xml

--- app/src/main/java/com/example/survirun/Fragmnet/UserFragment.java
+++ app/src/main/java/com/example/survirun/Fragmnet/UserFragment.java
@@ -23,36 +23,19 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class UserFragment extends Fragment {
-
     FragmentUserBinding binding;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    public  String uid= FirebaseAuth.getInstance().getUid();
+    private static final String DEFAULT_PATTERN = "%d%%";
 
     public UserFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static UserFragment newInstance(String param1, String param2) {
         UserFragment fragment = new UserFragment();
         Bundle args = new Bundle();
@@ -70,7 +53,6 @@ public class UserFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-    public  String uid= FirebaseAuth.getInstance().getUid();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -78,6 +60,7 @@ public class UserFragment extends Fragment {
 
         binding = FragmentUserBinding.inflate(inflater, container, false);
         View view=binding.getRoot();
+
         List<UserModel> userModels=new ArrayList<>();
         userModels.clear();
         FirebaseDatabase.getInstance().getReference().child("UserProfile").child(uid).addValueEventListener(new ValueEventListener() {
@@ -98,4 +81,5 @@ public class UserFragment extends Fragment {
 
         return view ;
     }
+
 }
