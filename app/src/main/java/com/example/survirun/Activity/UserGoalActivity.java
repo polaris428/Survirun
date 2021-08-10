79028796d41bea99bfdb9df4b131a5d62640e02c
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-08-10 17:44:45 +0900

    수정

--- app/src/main/java/com/example/survirun/Activity/UserGoalActivity.java
+++ app/src/main/java/com/example/survirun/Activity/UserGoalActivity.java
@@ -7,9 +7,14 @@ import android.os.Bundle;
 
 import com.example.survirun.R;
 import com.example.survirun.databinding.ActivityUserGoalBinding;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserGoalActivity extends AppCompatActivity {
     private ActivityUserGoalBinding binding;
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
