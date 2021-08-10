7c790731271f446a9cda12c147cea774085819e7
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-08-11 01:50:18 +0900

    유저 목표 수정

--- app/src/main/java/com/example/survirun/Activity/UserGoalActivity.java
+++ app/src/main/java/com/example/survirun/Activity/UserGoalActivity.java
@@ -3,7 +3,9 @@ package com.example.survirun.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.survirun.R;
 import com.example.survirun.databinding.ActivityUserGoalBinding;
@@ -13,18 +15,43 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class UserGoalActivity extends AppCompatActivity {
     private ActivityUserGoalBinding binding;
-    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    DatabaseReference databaseReference = firebaseDatabase.getReference();
-
+    int calorie, time, km;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUserGoalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        SharedPreferences sf = getSharedPreferences("goal", MODE_PRIVATE);
+        calorie = sf.getInt("calorie", 0);
+        time = sf.getInt("time", 0);
+        km = sf.getInt("km", 0);
+
+        if(calorie!=0){
+            set();
+        }
+        else {
+            SharedPreferences.Editor editor = sf.edit();
+            editor.putInt("calorie", 400);
+            editor.putInt("time", 60);
+            editor.putInt("km", 5);
+            editor.apply();
+            editor.commit();
+            set();
+        }
+
+
 
         binding.btnRetouch.setOnClickListener(v -> {
             Intent intent = new Intent(UserGoalActivity.this, UserGoalModifyActivity.class);
             startActivity(intent);
         });
+
+    }
+    private void set(){
+        String h = String.valueOf(time/60);
+        String m = String.valueOf(time%60);
+        binding.tvCalorie.setText(String.valueOf(calorie));
+        binding.tvTime.setText(h+"h "+m+"m");
+        binding.tvKm.setText(String.valueOf(km));
     }
 }
--- app/src/main/java/com/example/survirun/Activity/UserGoalModifyActivity.java
+++ app/src/main/java/com/example/survirun/Activity/UserGoalModifyActivity.java
@@ -2,18 +2,113 @@ package com.example.survirun.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.survirun.R;
 import com.example.survirun.databinding.ActivityUserGoalModifyBinding;
 
 public class UserGoalModifyActivity extends AppCompatActivity {
     private ActivityUserGoalModifyBinding binding;
+    boolean isCalorie = true;
+    boolean isTimeH = true;
+    boolean isTimeM = true;
+    boolean isKm = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUserGoalModifyBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        SharedPreferences sf = getSharedPreferences("goal", MODE_PRIVATE);
+        int calorie = sf.getInt("calorie", 0);
+        int time = sf.getInt("time", 0);
+        int km = sf.getInt("km", 0);
+
+        binding.etCalorie.setText(String.valueOf(calorie));
+        binding.etH.setText(String.valueOf(time/60));
+        binding.etM.setText(String.valueOf(time%60));
+        binding.etKm.setText(String.valueOf(km));
+
+        binding.etCalorie.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(binding.etCalorie.getText().toString().replace(" ", "").equals("")) isCalorie = false;
+                else isCalorie = true;
+            }
+        });
+
+        binding.etH.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    if(Integer.parseInt(binding.etH.getText().toString())>23) binding.etH.setText(String.valueOf(23));
+                } catch (Exception e) {
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(binding.etH.getText().toString().replace(" ", "").equals("")) isTimeH = false;
+                else isTimeH = true;
+            }
+        });
+        binding.etM.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    if(Integer.parseInt(binding.etM.getText().toString())>59) binding.etM.setText(String.valueOf(59));
+                } catch (Exception e) {
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(binding.etM.getText().toString().replace(" ", "").equals("")) isTimeM = false;
+                else isTimeM = true;
+            }
+        });
+        binding.etKm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(binding.etKm.getText().toString().replace(" ", "").equals("")) isKm = false;
+                else isKm = true;
+            }
+        });
+
+        binding.btnSave.setOnClickListener(v -> {
+            if(isCalorie&&isTimeH&&isTimeM&&isKm){
+                int inputCalorie = Integer.parseInt(binding.etCalorie.getText().toString());
+                int inputTime = Integer.parseInt(binding.etH.getText().toString())*60+Integer.parseInt(binding.etM.getText().toString());
+                int inputKm = Integer.parseInt(binding.etKm.getText().toString());
+                SharedPreferences.Editor editor= sf.edit();
+                editor.putInt("calorie", inputCalorie);
+                editor.putInt("time", inputTime);
+                editor.putInt("km", inputKm);
+                editor.apply();
+                editor.commit();
+                Intent intent = new Intent(UserGoalModifyActivity.this, UserGoalActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "빈칸을 채워주세요", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
