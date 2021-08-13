2389f268f6759f0d75bc5e3a5445176306cc0c4d
polairs428 <iou1056212@naver.com>
2021-08-13 22:05:29 +0900

    막대 그래프 기능 추가

--- app/src/main/java/com/example/survirun/Fragmnet/Fragment2.java
+++ app/src/main/java/com/example/survirun/Fragmnet/Fragment2.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 
 import com.example.survirun.databinding.Fragment2Binding;
 
+import org.eazegraph.lib.models.BarModel;
+
 public class Fragment2 extends Fragment {
     Fragment2Binding binding;
     @Override
@@ -16,6 +18,20 @@ public class Fragment2 extends Fragment {
                              Bundle savedInstanceState) {
         binding = Fragment2Binding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        binding.tab1Chart2.clearChart();
+
+        binding.tab1Chart2.addBar(new BarModel("12", 10f, 0xFF56B7F1));
+        binding.tab1Chart2.addBar(new BarModel("14", 10f, 0xFF56B7F1));
+        binding.tab1Chart2.addBar(new BarModel("15", 20f, 0xFF56B7F1));
+        binding.tab1Chart2.addBar(new BarModel("16", 10f, 0xFF56B7F1));
+        binding.tab1Chart2.addBar(new BarModel("17", 10f, 0xFF56B7F1));
+        binding.tab1Chart2.addBar(new BarModel("16", 10f, 0xFF56B7F1));
+        binding.tab1Chart2.addBar(new BarModel("17", 10f, 0xFF56B7F1));
+
+        binding.tab1Chart2.startAnimation();
+
+
+
         return view;
     }
 }
