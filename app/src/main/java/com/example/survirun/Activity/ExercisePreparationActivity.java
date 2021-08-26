c47eb45b16a62bc8ea8fc699ee38b3a947d8d3c2
polaris428 <iou1056212@naver.com>
2021-08-26 23:06:50 +0900

    운동 준비화면에서 맵 엑티비티로 전환

--- app/src/main/java/com/example/survirun/Activity/ExercisePreparationActivity.java
+++ app/src/main/java/com/example/survirun/Activity/ExercisePreparationActivity.java
@@ -2,15 +2,30 @@ package com.example.survirun.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.survirun.R;
+import com.example.survirun.databinding.ActivityExercisePreparationBinding;
+import com.example.survirun.databinding.ActivityLoginBinding;
 
 public class ExercisePreparationActivity extends AppCompatActivity {
-
+    ActivityExercisePreparationBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise_preparation);
+        binding = ActivityExercisePreparationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.exerciseStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(ExercisePreparationActivity.this,MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
