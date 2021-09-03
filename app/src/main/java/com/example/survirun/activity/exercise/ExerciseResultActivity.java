cb7deed296a8f327766ead00a8d29236154fe432
polaris428 <iou1056212@naver.com>
2021-09-03 21:54:32 +0900

    서버에서 이전 운동 데이터 가져오고 더한뒤 보내기

--- app/src/main/java/com/example/survirun/activity/exercise/ExerciseResultActivity.java
+++ app/src/main/java/com/example/survirun/activity/exercise/ExerciseResultActivity.java
@@ -9,11 +9,13 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.survirun.Medel.ScoreModel;
 import com.example.survirun.Medel.UserModel;
 import com.example.survirun.R;
 import com.example.survirun.activity.MainActivity;
 import com.example.survirun.databinding.ActivityExercisePreparationBinding;
 import com.example.survirun.databinding.ActivityExerciseResultBinding;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -21,13 +23,18 @@ import com.google.firebase.database.ValueEventListener;
 
 public class ExerciseResultActivity extends AppCompatActivity {
     private ActivityExerciseResultBinding binding;
+    ScoreModel scoreModel;
+    int previousCalories;
+    double  previousKM;
+    int previousTime;
     @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityExerciseResultBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        String uid= FirebaseAuth.getInstance().getUid();
+        scoreModel=new ScoreModel();
         int kcal = getIntent().getIntExtra("kcal",0);
         double km = getIntent().getDoubleExtra("walkedDistanceToKm",0);
         int timeToSec = getIntent().getIntExtra("timeToSec",0);
@@ -41,9 +48,32 @@ public class ExerciseResultActivity extends AppCompatActivity {
         binding.goMainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                scoreModel=new ScoreModel();
+                scoreModel.todayCalorie=previousCalories+kcal;
+                scoreModel.todayExerciseTime=previousTime+timeToSec;
+                scoreModel.todayKm=previousKM+km;
+                FirebaseDatabase.getInstance().getReference().child("UserProfile").child(uid).setValue(scoreModel);
                 startActivity(new Intent(ExerciseResultActivity.this, MainActivity.class));
             }
         });
 
+        FirebaseDatabase.getInstance().getReference().child("UserProfile").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                scoreModel = snapshot.getValue(ScoreModel.class);
+                previousCalories=scoreModel.todayCalorie;
+                previousKM=scoreModel.todayKm;
+                previousTime=scoreModel.todayExerciseTime;
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+
+            }
+        });
     }
 }
