1a0ea3eecdaf3ff73cb5069829844c8228c24ded
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-09-08 00:14:25 +0900

    자동로그인 옮김

--- app/src/main/java/com/example/survirun/activity/account/SplashActivity.java
+++ app/src/main/java/com/example/survirun/activity/account/SplashActivity.java
@@ -27,83 +27,30 @@ import java.util.Date;
 
 
 public class SplashActivity extends AppCompatActivity {
-    FirebaseAuth firebaseAuth;
-    int today;
-    int saveDay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        if(ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED) {
 
             requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
         }
-        SharedPreferences sf = getSharedPreferences("Login", MODE_PRIVATE);
-        String email = sf.getString("email", "");
-        String pwe = sf.getString("pwe", "");
-        saveDay = sf.getInt("day", 0);
-
-        if(today>saveDay){
-            sf.edit().putInt("day",today).commit();
-            Log.d("asdf",today+"");
-        }
-        if (!email.equals("") && !pwe.equals("") ) {
-            firebaseAuth = FirebaseAuth.getInstance();
-            firebaseAuth.signInWithEmailAndPassword(email, pwe)
-                    .addOnCompleteListener(SplashActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {//성공했을때
-                                getToday();
-                                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(SplashActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(intent);
-                            }
-                        }
-                    });
-
-        } else {
-            Handler mHandler = new Handler();
-            mHandler.postDelayed(new Runnable()  {
-                public void run() {
-                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
-                    finish();
-                }
-            }, 3000); // 0.5초후
-
-        }
-
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // 0.5초후
 
 
     }
-    public void getToday(){
-        //오늘 날짜 구하는 함수
-        long now = System.currentTimeMillis();
-        Date date = new Date(now);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd");
-        String getDay = sdf.format(date);
-        today=Integer.parseInt(getDay);
-        String uid=FirebaseAuth.getInstance().getUid();
-        if(today>saveDay){
-            UserModel userModel;
-            userModel = new UserModel();
-            userModel.todayExerciseTime = 0;
-            userModel.todayKm = 0.00;
-            userModel.todayCalorie = 0;
-            FirebaseDatabase.getInstance().getReference().child("UserProfile").child(uid).setValue(userModel);
-        }
 
 
-
-    }
 
 }
 
