e2b08144442ed4bd91182b01e0e980aa718d445a
polaris428 <iou1056212@naver.com>
2021-09-17 23:57:23 +0900

    사진 파이어베이스에서 자체 서버로 올리는 기능

--- app/src/main/java/com/example/survirun/activity/account/SignUpProfileActivity.java
+++ app/src/main/java/com/example/survirun/activity/account/SignUpProfileActivity.java
@@ -2,35 +2,58 @@ package com.example.survirun.activity.account;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.graphics.Typeface;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.survirun.R;
+import com.example.survirun.data.ResultData;
 import com.example.survirun.databinding.ActivitySignUpProfileBinding;
+import com.example.survirun.server.ServerClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.http.Multipart;
+
 public class SignUpProfileActivity extends AppCompatActivity {
+
+    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
     String name;
     String token;
+    String email;
     ActivitySignUpProfileBinding binding;
     private final int GET_GALLERY_IMAGE = 200;
     Uri selectedImageUri;
-
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
 
     @SuppressLint("ResourceAsColor")
@@ -40,6 +63,8 @@ public class SignUpProfileActivity extends AppCompatActivity {
         binding= ActivitySignUpProfileBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        mAuth = FirebaseAuth.getInstance();
+
 
         binding.profileImageview.setBackground(new ShapeDrawable(new OvalShape()));
         binding.profileImageview.setClipToOutline(true);
@@ -54,6 +79,7 @@ public class SignUpProfileActivity extends AppCompatActivity {
         SharedPreferences sf = getSharedPreferences("Login", MODE_PRIVATE);    // test 이름의 기본모드 설정
         token = sf.getString("token", "");
         name=sf.getString("name","");
+        email = sf.getString("email", "");
         binding.profileImageview.setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_PICK);
             intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
@@ -62,11 +88,24 @@ public class SignUpProfileActivity extends AppCompatActivity {
 
         binding.nextButton.setOnClickListener(v -> {
             if(selectedImageUri!=null) {
-                FirebaseStorage.getInstance().getReference().child("userImages").child(token).putFile(selectedImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+
+                File file = new File(getRealPathFromURI(selectedImageUri));
+                MultipartBody.Part body1 = prepareFilePart("image", selectedImageUri);
+                Call<ResultData>call= ServerClient.getServerService().postProfile(token,body1);
+
+                call.enqueue(new Callback<ResultData>() {
                     @Override
-                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                        Intent intent = new Intent(SignUpProfileActivity.this, ResultActivity.class);
-                        startActivity(intent);
+                    public void onResponse(Call<ResultData> call, Response<ResultData> response) {
+                        if(response.isSuccessful()){
+                            Log.d("asdf","성공");
+                        }else{
+                            Log.d("adsf",response.errorBody().toString());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResultData> call, Throwable t) {
+                        t.printStackTrace();
                     }
                 });
             }
@@ -85,6 +124,28 @@ public class SignUpProfileActivity extends AppCompatActivity {
         if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
             selectedImageUri = data.getData();
             Glide.with(getApplicationContext()).load(selectedImageUri).into(binding.profileImageview);
+
         }
     }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(this, contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+    @NonNull
+    private MultipartBody.Part prepareFilePart(String partName, Uri fileUri) {
+        File file = new File(getRealPathFromURI(fileUri));
+        RequestBody requestFile = RequestBody.create(MediaType.parse(MULTIPART_FORM_DATA), file);
+        return MultipartBody.Part.createFormData(partName, file.getName(), requestFile);
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/survirun/server/ServiceService.java
+++ app/src/main/java/com/example/survirun/server/ServiceService.java
@@ -1,18 +1,26 @@
 package com.example.survirun.server;
 
+import android.net.Uri;
+
 import com.example.survirun.data.ExerciseData;
 import com.example.survirun.data.LoginData;
 import com.example.survirun.data.NewUserData;
 import com.example.survirun.data.ResultData;
 import com.example.survirun.data.TokenData;
 
+import java.io.File;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface ServiceService {
@@ -25,6 +33,11 @@ public interface ServiceService {
     @PATCH("/api/v1/auth/by-username/:{username}")
     Call<ResultData>inputName(@Path("username")String name,@Header("x-access-token")String token);
 
-    @GET("api/v1/exercise")
+    @GET("/api/v1/exercise")
     Call<ExerciseData>getExercise(@Header("x-access-token")String token);
+
+    @Multipart
+    @POST("/api/v1/auth/profile")
+    Call<ResultData>postProfile(@Header("x-access-token")String token,@Part MultipartBody.Part file );
+
 }
