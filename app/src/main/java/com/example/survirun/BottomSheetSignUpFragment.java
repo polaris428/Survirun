8112f9d8763145d72eaf50795c701c1c42dd21e9
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-09-24 23:31:48 +0900

    프로필 사진 넣는 코드 수정

---
+++ app/src/main/java/com/example/survirun/BottomSheetSignUpFragment.java
@@ -0,0 +1,38 @@
+package com.example.survirun;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.survirun.databinding.FragmentBottomSheetSignUpBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+
+public class BottomSheetSignUpFragment extends BottomSheetDialogFragment {
+    FragmentBottomSheetSignUpBinding binding;
+    private BottomSheetListener mListener;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentBottomSheetSignUpBinding.inflate(inflater, container, false);
+        View v = binding.getRoot();
+        mListener = (BottomSheetListener)getContext();
+        binding.gallery.setOnClickListener(view -> {
+            mListener.onClickGallery();
+        });
+        binding.camera.setOnClickListener(view -> {
+            mListener.onClickCamera();
+        });
+        binding.basics.setOnClickListener(view -> {
+            mListener.onClickBasics();
+        });
+        return v;
+    }
+    public interface BottomSheetListener{
+        void onClickGallery();
+        void onClickCamera();
+        void onClickBasics();
+    }
+}
--- app/src/main/java/com/example/survirun/activity/account/SignUpProfileActivity.java
+++ app/src/main/java/com/example/survirun/activity/account/SignUpProfileActivity.java
@@ -1,17 +1,27 @@
 package com.example.survirun.activity.account;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.loader.content.CursorLoader;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -21,6 +31,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.survirun.BottomSheetSignUpFragment;
 import com.example.survirun.R;
 import com.example.survirun.activity.MainActivity;
 import com.example.survirun.data.ResultData;
@@ -35,7 +46,9 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStream;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -45,17 +58,17 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.http.Multipart;
 
-public class SignUpProfileActivity extends AppCompatActivity {
+public class SignUpProfileActivity extends AppCompatActivity implements BottomSheetSignUpFragment.BottomSheetListener{
 
     public static final String MULTIPART_FORM_DATA = "multipart/form-data";
     String name;
     String token;
     String email;
     ActivitySignUpProfileBinding binding;
-    private final int GET_GALLERY_IMAGE = 200;
     Uri selectedImageUri;
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     SharedPreferences.Editor editor;
+    BottomSheetSignUpFragment signUpFragment;
 
     @SuppressLint("ResourceAsColor")
     @Override
@@ -67,6 +80,7 @@ public class SignUpProfileActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         SharedPreferences sf = getSharedPreferences("Login", MODE_PRIVATE);
         editor=sf.edit();
+        signUpFragment = new BottomSheetSignUpFragment();
 
         binding.profileImageview.setBackground(new ShapeDrawable(new OvalShape()));
         binding.profileImageview.setClipToOutline(true);
@@ -81,10 +95,9 @@ public class SignUpProfileActivity extends AppCompatActivity {
         token = sf.getString("token", "");
         name=sf.getString("name","");
         email = sf.getString("email", "");
+
         binding.profileImageview.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_PICK);
-            intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-            startActivityForResult(intent, GET_GALLERY_IMAGE);
+            signUpFragment.show(getSupportFragmentManager(), "bottomSheet");
         });
 
         binding.nextButton.setOnClickListener(v -> {
@@ -120,15 +133,37 @@ public class SignUpProfileActivity extends AppCompatActivity {
 
 
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            selectedImageUri = data.getData();
-            Glide.with(getApplicationContext()).load(selectedImageUri).into(binding.profileImageview);
-
+    public void onClickGallery() {//저장소 읽어오는 퍼미션
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }//퍼미션이 허용되지 않아 있다면 퍼미션 허용메세지 생성 & 코드0보내기
+        else{
+            setGallery();//퍼미션이 허용되어 있다면 갤러리불러오기
         }
+        signUpFragment.dismiss();
+    }
+
+    @Override
+    public void onClickCamera() {//카메라 퍼미션
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);//코드 1보내기
+        }
+        else{
+            setCamera();
+        }
+
+        signUpFragment.dismiss();
+    }
+
+    @Override
+    public void onClickBasics() {//기본이미지 선택했을때 drawable에 있는거 불러오기
+        /*Glide.with(getApplicationContext()).load(R.drawable.ic_profile).into(binding.profileImageview);
+        selectedImageUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                "://" + getResources().getResourcePackageName(R.drawable.ic_profile)
+                + '/' + getResources().getResourceTypeName(R.drawable.ic_profile) + '/' + getResources().getResourceEntryName(R.drawable.ic_profile) );
+        signUpFragment.dismiss();*/
     }
 
     private String getRealPathFromURI(Uri contentUri) {
@@ -141,6 +176,88 @@ public class SignUpProfileActivity extends AppCompatActivity {
         cursor.close();
         return result;
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //퍼미션허용 메세지를 보냈을떄
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            // requestPermission의 배열의 index가 아래 grantResults index와 매칭
+            // 퍼미션이 승인되면
+            if(grantResults.length > 0  && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                if(requestCode==0){//위에서 코드 0을 보냄
+                    setGallery();
+                }
+                else if(requestCode==1){//코드 1
+                    setCamera();
+                }
+
+            }
+            // 퍼미션이 승인 거부되면
+            else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                    //퍼미션 2번까진 아래코드
+                    Toast.makeText(SignUpProfileActivity.this, "퍼미션이 거부되었습니다. 다시 눌러 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
+
+
+                }else {
+                    Toast.makeText(SignUpProfileActivity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
+                    //2번이후론 알아서
+                }
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {//코드 0번 갤러리
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    selectedImageUri = getImageUri(this, img);
+                    Glide.with(getApplicationContext()).load(img).into(binding.profileImageview);
+                } catch (Exception e) {
+
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
+        if(requestCode == 1){//코드 1번 카메라
+            if(resultCode == RESULT_OK){
+                try{
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    selectedImageUri = getImageUri(this, imageBitmap);
+                    Glide.with(getApplicationContext()).load(imageBitmap).into(binding.profileImageview);
+                }catch (Exception e){
+
+                }
+            }
+        }
+    }
+    private void setGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 0);
+    }
+    private void setCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+    public Uri getImageUri(Context ctx, Bitmap bitmap) {//비트맵 Uri로 변환
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage
+                (ctx.getContentResolver(),
+                        bitmap, "Temp", null);
+        return Uri.parse(path);
+    }
+
     @NonNull
     private MultipartBody.Part prepareFilePart(String partName, Uri fileUri) {
         File file = new File(getRealPathFromURI(fileUri));
