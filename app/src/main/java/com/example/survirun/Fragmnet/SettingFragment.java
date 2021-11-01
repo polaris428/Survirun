e76189bf8d43530716d51a4d14a1b7dec150d327
polaris428 <iou1056212@naver.com>
2021-11-01 11:34:53 +0900

    그리들 캐싱 데이터

--- app/src/main/java/com/example/survirun/Fragmnet/SettingFragment.java
+++ app/src/main/java/com/example/survirun/Fragmnet/SettingFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.survirun.R;
 import com.example.survirun.data.ImageData;
 import com.example.survirun.databinding.FragmentSettingBinding;
@@ -56,6 +57,7 @@ public class SettingFragment extends Fragment {
                     Glide.with(SettingFragment.this)
                             .load("https://dicon21.2tle.io/api/v1/image?reqType=profile&id=" + response.body().img)
                             .circleCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
                             .error(R.drawable.ic_profile)
                             .into(binding.profileImageview);
                 }
