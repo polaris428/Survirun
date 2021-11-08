11c7a08ec485729269b001e51a4a062d05df0876
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-11-08 17:50:25 +0900

    친구 팝업페이지

--- app/src/main/java/com/example/survirun/Fragmnet/Friend/FriendInformationActivity.java
+++ app/src/main/java/com/example/survirun/Fragmnet/Friend/FriendInformationActivity.java
@@ -2,15 +2,27 @@ package com.example.survirun.Fragmnet.Friend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.example.survirun.R;
+import com.example.survirun.databinding.ActivityFriendInformationBinding;
 
-public class FriendInformationActivity extends AppCompatActivity {
+public class FriendInformationActivity extends Activity {
+    private ActivityFriendInformationBinding binding;
+    String result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friend_information);
+        binding = ActivityFriendInformationBinding.inflate(getLayoutInflater());
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(binding.getRoot());
+        Intent intent = getIntent();
+        result = intent.getStringExtra("list");
+        binding.text.setText(result);
     }
 }
