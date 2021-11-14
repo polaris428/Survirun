7c73fdc4470287270be4649e41541b2f7e847d9a
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-11-14 23:11:17 +0900

    친구 찾을때 애니메이션 추가

--- app/src/main/java/com/example/survirun/Fragmnet/Friend/FriendFragment.java
+++ app/src/main/java/com/example/survirun/Fragmnet/Friend/FriendFragment.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -29,6 +31,7 @@ import com.example.survirun.data.ResultData;
 import com.example.survirun.data.getUserData;
 import com.example.survirun.databinding.FragmentFriendBinding;
 import com.example.survirun.server.ServerClient;
+import com.google.android.material.progressindicator.BaseProgressIndicator;
 
 
 import java.util.ArrayList;
@@ -65,8 +68,12 @@ public class FriendFragment extends Fragment {
         friendDB = FriendDB.getInstance(getContext());
         mContext = getContext().getApplicationContext();
         friendAdapter = new FriendAdapter(friendRoomList);
+        binding.cardView.setVisibility(View.GONE);
 
+        Animation animation = AnimationUtils.loadAnimation(getContext(),R.anim.rotate);
         checkFriends();
+
+        binding.image.startAnimation(animation);
 
 
         Log.d("토큰", token);
@@ -81,13 +88,15 @@ public class FriendFragment extends Fragment {
         binding.findFriends.setOnClickListener(v -> {
             friendEmail = binding.emileInputEditText.getText().toString();
             if (email.equals(friendEmail)) {
-                Toast.makeText(getContext(), "자기 자신의 이미 최고의 친구입니다", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), R.string.you_already_friend, Toast.LENGTH_LONG).show();
             } else if (binding.emileInputEditText.getText().toString().equals("")) {
-                Toast.makeText(getContext(), "공백을 채워주세요", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), R.string.fill_error, Toast.LENGTH_LONG).show();
             } else {
+                binding.findFriendsLoading.setVisibility(View.VISIBLE);
+                binding.backButton.setVisibility(View.GONE);
                 binding.findFriendsCardView.setVisibility(View.GONE);
                 binding.friendsError.setVisibility(View.GONE);
-                binding.cardView.setVisibility(View.GONE);
+                binding.cardView.setVisibility(View.VISIBLE);
                 Call<getUserData> call1 = ServerClient.getServerService().getUser(token, binding.emileInputEditText.getText().toString());
                 call1.enqueue(new Callback<getUserData>() {
                     @Override
@@ -98,9 +107,11 @@ public class FriendFragment extends Fragment {
                             binding.cardView.setVisibility(View.VISIBLE);
                             binding.findFriendsCardView.setVisibility(View.VISIBLE);
                             binding.friendsError.setVisibility(View.GONE);
+                            binding.findFriendsLoading.setVisibility(View.GONE);
+                            binding.backButton.setVisibility(View.VISIBLE);
                             binding.usernameTextview.setText(name);
                             ExerciseHistory exerciseHistory = response.body().exerciseHistory.get(0);
-                            binding.exerciseTextview.setText(exerciseHistory.calorie + "칼로리 " + exerciseHistory.km + "킬로미터 " + exerciseHistory.time + "시간 ");
+                            binding.exerciseTextview.setText(exerciseHistory.calorie + "kcal " + exerciseHistory.km + "km " + exerciseHistory.time + getString(R.string.hour));
                             Call<ImageData> getProfile = ServerClient.getServerService().getSuchProfile(token, "username", "url", response.body().username);
                             getProfile.enqueue(new Callback<ImageData>() {
                                 @Override
@@ -118,6 +129,8 @@ public class FriendFragment extends Fragment {
                                         profile = "https://dicon21.2tle.io/api/v1/image?reqType=profile&id=" + response.body().img;
 
                                     } else {
+                                        binding.findFriendsLoading.setVisibility(View.GONE);
+                                        binding.backButton.setVisibility(View.VISIBLE);
                                         binding.cardView.setVisibility(View.VISIBLE);
                                         binding.friendsError.setVisibility(View.VISIBLE);
                                     }
@@ -129,10 +142,10 @@ public class FriendFragment extends Fragment {
                                 }
                             });
                         } else {
-
+                            binding.findFriendsLoading.setVisibility(View.GONE);
+                            binding.backButton.setVisibility(View.VISIBLE);
                             binding.cardView.setVisibility(View.VISIBLE);
                             binding.friendsError.setVisibility(View.VISIBLE);
-
                         }
                     }
 
