ab2c52e1eae1a722fedd8fa6ecf38f9c83e26213
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-11-15 16:29:37 +0900

    프로그래스 애니메이션 추가

--- app/src/main/java/com/example/survirun/activity/account/ProgressDialog.java
+++ app/src/main/java/com/example/survirun/activity/account/ProgressDialog.java
@@ -2,7 +2,9 @@ package com.example.survirun.activity.account;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.view.Window;
+import android.widget.ImageView;
 
 import com.example.survirun.R;
 
@@ -11,5 +13,10 @@ public class ProgressDialog extends Dialog {
         super(context);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_progress_dialog);
+
+        ImageView iv = findViewById(R.id.imageView);
+        final AnimationDrawable drawable =
+                (AnimationDrawable) iv.getBackground();
+        drawable.start();
     }
 }
