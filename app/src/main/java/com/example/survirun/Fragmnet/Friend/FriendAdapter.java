5a219f63a3b52904d627cee38a68e0b7487a2e6a
polaris428 <iou1056212@naver.com>
2021-11-15 17:30:46 +0900

    친두 아이템 디자인 변경 1차

--- app/src/main/java/com/example/survirun/Fragmnet/Friend/FriendAdapter.java
+++ app/src/main/java/com/example/survirun/Fragmnet/Friend/FriendAdapter.java
@@ -6,34 +6,30 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.model.ModelLoader;
 import com.example.survirun.R;
+import com.example.survirun.activity.account.SignInActivity;
+import com.example.survirun.activity.account.SignUpProfileActivity;
 import com.example.survirun.data.ExerciseHistory;
 import com.example.survirun.data.FriendRoom;
-import com.example.survirun.data.Friends;
 import com.example.survirun.data.ImageData;
 import com.example.survirun.data.getUserData;
 import com.example.survirun.server.ServerClient;
 
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -46,6 +42,7 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
 
     Context context;
     String token;
+    String profile;
 
     public FriendAdapter(List<FriendRoom> list) {
         friendRoomList = list;
@@ -70,30 +67,36 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
         FriendRoom item = friendRoomList.get(position);
         holder.email.setText(item.email);
         holder.name.setText(item.name);
-        Glide.with(context).load(item.profile)
+        Glide.with(context).load("https://dicon21.2tle.io/api/v1/image?reqType=profile&id="+item.profile)
                 .error(R.drawable.userdefaultprofile)
                 .circleCrop()
                 .into(holder.profile);
+
+        Log.d("adsf",item.profile);
         holder.detailButton.setOnClickListener(v -> {
 
-
             Intent intent = new Intent(context, FriendInformationActivity.class);
-            intent.putExtra("list", "값");
-            context.startActivity(intent);
-        });
+            intent.putExtra("userName",holder.name.getText().toString());
+            intent.putExtra("userEmail",holder.email.getText().toString());
+            intent.putExtra("profile",item.profile);
 
-        Call<getUserData> call1 = ServerClient.getServerService().getUser(token, holder.name.getText().toString());
+            context.startActivity(intent);
+
+        });
+        Call<getUserData>call1=ServerClient.getServerService().getUser(token, item.email);
         call1.enqueue(new Callback<getUserData>() {
             @Override
             public void onResponse(Call<getUserData> call, Response<getUserData> response) {
-                if (response.isSuccessful()) {
+                if(response.isSuccessful()){
 
-                    ExerciseHistory exerciseHistory = response.body().exerciseHistory.get(0);
-                    holder.name.setText(response.body().username);
-                    Log.d("ad", response.body().username);
+                    ExerciseHistory exerciseHistory=response.body().exerciseHistory.get(0);
+                    holder.exerciseKcalTextview.setText(String.valueOf(exerciseHistory.calorie));
+                    holder.exerciseTimeTextView.setText(String.valueOf(exerciseHistory.time));
+                    holder.exerciseKmTextView.setText(String.valueOf(exerciseHistory.km));
+                    Log.d("ad",response.body().username);
 
-                } else {
-                    Log.e("adsf", "실패" + response.code());
+                }else{
+                    Log.e("adsf","실패"+ response.code());
 
                 }
             }
@@ -104,51 +107,9 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
             }
         });
 
-        holder.detailButton.setOnClickListener(v -> {
-            Intent intent = new Intent(context, FriendInformationActivity.class);
-            intent.putExtra("list", "값");
-            context.startActivity(intent);
-        });
-
-        Call<getUserData> call2 = ServerClient.getServerService().getUser(token, holder.email.getText().toString());
-        call2.enqueue(new Callback<getUserData>() {
-            @Override
-            public void onResponse(Call<getUserData> call, Response<getUserData> response) {
-                if (response.isSuccessful()) {
 
 
-                    ExerciseHistory exerciseHistory = Objects.requireNonNull(response.body()).exerciseHistory.get(0);
-                    holder.exerciseTextview.setText(exerciseHistory.calorie + "칼로리" + exerciseHistory.km + "킬로미터" + exerciseHistory.time + "운동시간");
-                    Log.d("adsf", exerciseHistory.km + "");
-                    Call<ImageData> getProfile = ServerClient.getServerService().getSuchProfile(token, "username", "url", response.body().username);
-                    getProfile.enqueue(new Callback<ImageData>() {
-                        @Override
-                        public void onResponse(Call<ImageData> call, Response<ImageData> response) {
-                            if (response.isSuccessful()) {
-                                Log.d("d", response.body().img);
-                                Glide.with(context)
-                                        .load("https://dicon21.2tle.io/api/v1/image?reqType=profile&id=" + response.body().img)
-                                        .error(R.drawable.userdefaultprofile)
-                                        .circleCrop()
-                                        .into(holder.profile);
-                            }
-                        }
 
-                        @Override
-                        public void onFailure(Call<ImageData> call, Throwable t) {
-                            t.printStackTrace();
-                        }
-                    });
-                } else {
-                    Log.d("adsf", "실패");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<getUserData> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
 
     }
 
@@ -161,7 +122,9 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
         TextView name;
         TextView email;
         ImageView profile;
-        TextView exerciseTextview;
+        TextView exerciseKcalTextview;
+        TextView exerciseTimeTextView;
+        TextView exerciseKmTextView;
         ConstraintLayout constraintLayout1;
         ConstraintLayout constraintLayout2;
         Button detailButton;
@@ -174,7 +137,9 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
             profile = itemView.findViewById(R.id.profile_imageview);
             constraintLayout1 = itemView.findViewById(R.id.constraint_layout);
             constraintLayout2 = itemView.findViewById(R.id.card_item2);
-            exerciseTextview = itemView.findViewById(R.id.exercise_textview);
+            exerciseKcalTextview = itemView.findViewById(R.id.exercise_kcal_text_view);
+            exerciseTimeTextView=itemView.findViewById(R.id.exercise_time_text_view);
+            exerciseKmTextView=itemView.findViewById(R.id.exercise_km_text_view);
             detailButton = itemView.findViewById(R.id.detail_button);
             context = itemView.getContext();
 
