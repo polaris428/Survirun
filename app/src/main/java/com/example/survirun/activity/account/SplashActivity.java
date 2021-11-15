fd22886bdd13ef00f9e16518d23a2b07586b1092
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-11-15 23:43:10 +0900

    다이얼로그 수정

--- app/src/main/java/com/example/survirun/activity/account/SplashActivity.java
+++ app/src/main/java/com/example/survirun/activity/account/SplashActivity.java
@@ -62,7 +62,7 @@ public class SplashActivity extends AppCompatActivity {
         dialog = new Dialog(SplashActivity.this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.setContentView(R.layout.dialog);
+        dialog.setContentView(R.layout.network_dialog);
 
         final AnimationDrawable drawableRun =
                 (AnimationDrawable) binding.runImageView.getBackground();
@@ -227,18 +227,11 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     void showDialog() {
-        TextView explain = dialog.findViewById(R.id.explain_textView);
         Button finishButton = dialog.findViewById(R.id.cancel_button);
         Button retryButton = dialog.findViewById(R.id.yes_button);
         Button helpButton=dialog.findViewById(R.id.help_button);
-        LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
-        params1.weight = 0.45f;
-        finishButton.setLayoutParams(params1);
-        explain.setText(R.string.network_error_occurred);
-        finishButton.setText(R.string.close);
-        retryButton.setText(R.string.retry);
-        dialog.show();
         dialog.setCancelable(false);
+        dialog.show();
         helpButton.setOnClickListener(v -> {
                     Intent email = new Intent(Intent.ACTION_SEND);
                     email.setType("plain/text");
