bf416794174a5bb582f25dc6843fb4e2b2a408dd
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-11-16 23:21:12 +0900

    친구 삭제

--- app/src/main/java/com/example/survirun/Fragmnet/Friend/FriendInformationActivity.java
+++ app/src/main/java/com/example/survirun/Fragmnet/Friend/FriendInformationActivity.java
@@ -1,40 +1,57 @@
 package com.example.survirun.Fragmnet.Friend;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.service.autofill.UserData;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
+import com.example.survirun.FriendDB;
 import com.example.survirun.R;
-import com.example.survirun.data.ExerciseRecordData;
+import com.example.survirun.activity.FriendActivity;
+import com.example.survirun.data.FriendRoom;
 import com.example.survirun.data.ResultData;
 import com.example.survirun.data.getUserData;
-
 import com.example.survirun.databinding.ActivityFriendInformationBinding;
 import com.example.survirun.server.ServerClient;
 
 import org.eazegraph.lib.models.BarModel;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class FriendInformationActivity extends Activity {
+public class FriendInformationActivity extends AppCompatActivity {
     private ActivityFriendInformationBinding binding;
     String userName;
     String userEmile;
     String userProfile;
+    String token;
+
+    Dialog dialog;
+
+    private List<FriendRoom> friendRoomList;
+    private FriendDB friendDB = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,73 +59,69 @@ public class FriendInformationActivity extends Activity {
         binding = ActivityFriendInformationBinding.inflate(getLayoutInflater());
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(binding.getRoot());
+
+        friendDB = FriendDB.getInstance(this);
+
         Intent getIntent = getIntent();
-        userName=getIntent.getStringExtra("userName");
-        userEmile=getIntent.getStringExtra("userEmail");
-        userProfile=getIntent.getStringExtra("profile");
-        SharedPreferences sf = getSharedPreferences("Login",MODE_PRIVATE);
-        String token = sf.getString("token", "");
-        Log.d("프로필",userProfile);
+        userName = getIntent.getStringExtra("userName");
+        userEmile = getIntent.getStringExtra("userEmail");
+        userProfile = getIntent.getStringExtra("profile");
+
+        SharedPreferences sf = getSharedPreferences("Login", MODE_PRIVATE);
+        token = sf.getString("token", "");
+        Log.d("프로필", userProfile);
+
         binding.usernameTextview.setText(userName);
         binding.userEmailTextView.setText(userEmile);
-        Glide.with(this).load("https://dicon21.2tle.io/api/v1/image?reqType=profile&id="+userProfile)
+        Glide.with(this).load("https://dicon21.2tle.io/api/v1/image?reqType=profile&id=" + userProfile)
                 .error(R.drawable.userdefaultprofile)
                 .circleCrop()
                 .into(binding.userProfileImageView);
-        binding.deleteFriendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Call<ResultData>call=ServerClient.getServerService().deleteFriend(token,"email",userEmile);
-                call.enqueue(new Callback<ResultData>() {
-                    @Override
-                    public void onResponse(Call<ResultData> call, Response<ResultData> response) {
-                        if (response.isSuccessful()){
-                            //여기서 부터 룸 삭제 코드
-                        }
-                    }
 
-                    @Override
-                    public void onFailure(Call<ResultData> call, Throwable t) {
-                        t.printStackTrace();
-                        Toast.makeText(FriendInformationActivity.this,R.string.server_error,Toast.LENGTH_LONG);
-                    }
-                });
-            }
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setContentView(R.layout.dialog);
+
+        binding.menuButton.setOnClickListener(this::showPopup);
+
+        binding.backButton.setOnClickListener(v -> {
+            finish();
         });
+
 
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
 
 
-
         binding.image.startAnimation(animation);
         binding.findRecord.setVisibility(View.VISIBLE);
-        Call<getUserData>call=ServerClient.getServerService().getUser(token,userEmile);
+        Call<getUserData> call = ServerClient.getServerService().getUser(token, userEmile);
         call.enqueue(new Callback<getUserData>() {
             @Override
             public void onResponse(Call<getUserData> call, Response<getUserData> response) {
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
                     binding.calorieBarGraph.clearChart();
                     binding.goalBarGraph.clearChart();
                     binding.kmBarGraph.clearChart();
                     binding.exerciseTimeBarGraph.clearChart();
-                    if(response.body().exerciseHistory.size()==1&&response.body().exerciseHistory.get(0).time==0){
+                    if (response.body().exerciseHistory.size() == 1 && response.body().exerciseHistory.get(0).time == 0) {
                         binding.noRecord.setVisibility(View.VISIBLE);
                         binding.findRecord.setVisibility(View.GONE);
 
 
-                    }else{
+                    } else {
                         binding.exerciseGraph.setVisibility(View.VISIBLE);
                         binding.ExerciseMessage.setVisibility(View.GONE);
 
-                        for (int i=0;i<response.body().exerciseHistory.size();i++){
-                            int calorie=response.body().exerciseHistory.get(i).calorie;
-                            double km= response.body().exerciseHistory.get(i).km;
-                            String data= response.body().exerciseHistory.get(i).date;
-                            int time=response.body().exerciseHistory.get(i).time;
-                            Log.d("시간",time/60+"");
+                        for (int i = 0; i < response.body().exerciseHistory.size(); i++) {
+                            int calorie = response.body().exerciseHistory.get(i).calorie;
+                            double km = response.body().exerciseHistory.get(i).km;
+                            String data = response.body().exerciseHistory.get(i).date;
+                            int time = response.body().exerciseHistory.get(i).time;
+                            Log.d("시간", time / 60 + "");
                             binding.kmBarGraph.addBar(new BarModel(data, (float) km, 0xFF56B7F1));
                             binding.calorieBarGraph.addBar(new BarModel(data, calorie, 0xFF56B7F1));
-                            binding.exerciseTimeBarGraph.addBar(new BarModel(data,time,0xFF56B7F1));
+                            binding.exerciseTimeBarGraph.addBar(new BarModel(data, time, 0xFF56B7F1));
                         }
                         binding.calorieBarGraph.startAnimation();
                         binding.goalBarGraph.startAnimation();
@@ -117,7 +130,7 @@ public class FriendInformationActivity extends Activity {
                     }
 
 
-                }else{
+                } else {
                     Toast.makeText(FriendInformationActivity.this, "서버오류", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -130,6 +143,68 @@ public class FriendInformationActivity extends Activity {
         });
 
 
+    }
 
+    private void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.friend_menu, popup.getMenu());
+        popup.show();
+        popup.getMenu().getItem(0).setOnMenuItemClickListener(item -> {
+            onOptionsItemSelected(item);
+            return true;
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.delete_friend) {
+            showDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialog() {
+        TextView explain = dialog.findViewById(R.id.explain_textView);
+        explain.setText(R.string.delete_friend_text);
+        dialog.show();
+        dialog.findViewById(R.id.cancel_button).setOnClickListener(v -> dialog.dismiss());
+        dialog.findViewById(R.id.yes_button).setOnClickListener(v -> {
+            deleteFriend();
+            startActivity(new Intent(this, FriendActivity.class));
+            finish();
+        });
+    }
+
+    private void deleteFriend() {
+        Call<ResultData> call = ServerClient.getServerService().deleteFriend(token, "email", userEmile);
+        call.enqueue(new Callback<ResultData>() {
+            @Override
+            public void onResponse(Call<ResultData> call, Response<ResultData> response) {
+                if (response.isSuccessful()) {
+                    class InsertRunnable implements Runnable {
+                        @Override
+                        public void run() {
+                            try {
+                                FriendRoom friendRoom = friendDB.friendDao().findById(userEmile);
+                                friendDB.friendDao().delete(friendRoom);
+                            } catch (Exception e) {
+
+                            }
+                        }
+                    }
+                    InsertRunnable insertRunnable = new InsertRunnable();
+                    Thread t = new Thread(insertRunnable);
+                    t.start();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResultData> call, Throwable t) {
+                t.printStackTrace();
+                Toast.makeText(FriendInformationActivity.this, R.string.server_error, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/survirun/FriendDao.java
+++ app/src/main/java/com/example/survirun/FriendDao.java
@@ -1,6 +1,7 @@
 package com.example.survirun;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -15,9 +16,15 @@ public interface FriendDao {
     @Query("SELECT * FROM friendroom")
     List<FriendRoom> getAll();
 
+    @Query("SELECT * FROM friendroom where email=:email")
+    FriendRoom findById(String email);
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(FriendRoom... friendRoom);
 
     @Update
     void update(FriendRoom... friendRoom);
+
+    @Delete
+    void delete(FriendRoom friendRoom);
 }
