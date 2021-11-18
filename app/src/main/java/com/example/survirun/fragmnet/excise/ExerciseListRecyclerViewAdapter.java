0f13932e9181aebe117d0a510e3a96e0c8cb56f7
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-11-19 01:47:12 +0900

    랭킹과 친구 수정

--- app/src/main/java/com/example/survirun/fragmnet/excise/ExerciseListRecyclerViewAdapter.java
+++ app/src/main/java/com/example/survirun/fragmnet/excise/ExerciseListRecyclerViewAdapter.java
@@ -66,12 +66,12 @@ public class ExerciseListRecyclerViewAdapter extends RecyclerView.Adapter<Exerci
         Handler handler = new Handler();
         holder.expandImageButton.setOnClickListener(view -> {
             if(holder.constraintLayout.getVisibility()==View.GONE){
-                ValueAnimator anim = ValueAnimator.ofInt(1, 500);
+                ValueAnimator anim = ValueAnimator.ofInt(1, holder.constraintLayout.getMaxHeight());
                 setAnimation(anim, holder.constraintLayout);
                 holder.expandImageButton.setImageResource(R.drawable.ic_upblack);
                 holder.constraintLayout.setVisibility(View.VISIBLE);
             }else {
-                ValueAnimator anim = ValueAnimator.ofInt(500, 1);
+                ValueAnimator anim = ValueAnimator.ofInt(holder.constraintLayout.getMaxHeight(), 1);
                 setAnimation(anim, holder.constraintLayout);
 
                 handler.postDelayed(new Runnable() {
@@ -80,7 +80,7 @@ public class ExerciseListRecyclerViewAdapter extends RecyclerView.Adapter<Exerci
                         holder.expandImageButton.setImageResource(R.drawable.ic_downblack);
                         holder.constraintLayout.setVisibility(View.GONE);
                     }
-                },800);
+                },600);
 
             }
         });
@@ -147,7 +147,7 @@ public class ExerciseListRecyclerViewAdapter extends RecyclerView.Adapter<Exerci
     }
 
     private void setAnimation(ValueAnimator anim, ConstraintLayout constraintLayout){
-        anim.setDuration(800);
+        anim.setDuration(600);
         anim.addUpdateListener(animation -> {
             Integer value = (Integer) animation.getAnimatedValue();
             constraintLayout.getLayoutParams().height = value.intValue();
