6d2b9c2df46ebaf33fa401ba67f240eaa095fc6a
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-12-28 23:33:59 +0900

    수정

--- app/src/main/java/com/example/survirun/activity/exercise/MultiActivity.java
+++ app/src/main/java/com/example/survirun/activity/exercise/MultiActivity.java
@@ -1,27 +1,15 @@
 package com.example.survirun.activity.exercise;
 
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
-import com.example.survirun.R;
 import com.example.survirun.databinding.ActivityMultiBinding;
 
 
 public class MultiActivity extends AppCompatActivity {
     ActivityMultiBinding binding;
-    Boolean  isFrontSurvivor = true,
-            isFrontResearcher = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
