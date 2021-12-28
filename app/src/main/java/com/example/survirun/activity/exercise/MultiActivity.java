562daeda9bdb254b14f61c39550facba87235907
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-12-28 23:29:51 +0900

    멀티 수정

--- app/src/main/java/com/example/survirun/activity/exercise/MultiActivity.java
+++ app/src/main/java/com/example/survirun/activity/exercise/MultiActivity.java
@@ -34,36 +34,5 @@ public class MultiActivity extends AppCompatActivity {
             startActivity(t);
         });
 
-        binding.survivorCardView.setOnLongClickListener(v -> {
-            if (isFrontSurvivor) {
-                binding.survivorFront.setVisibility(View.INVISIBLE);
-                binding.survivorBack.setVisibility(View.VISIBLE);
-                YoYo.with(Techniques.FadeIn).duration(300).playOn(binding.survivorBack);
-                isFrontSurvivor = false;
-            } else {
-                binding.survivorFront.setVisibility(View.VISIBLE);
-                binding.survivorBack.setVisibility(View.INVISIBLE);
-
-                YoYo.with(Techniques.FadeIn).duration(300).playOn(binding.survivorFront);
-                isFrontSurvivor = true;
-            }
-            return true;
-        });
-        binding.researcherCardView.setOnLongClickListener(v -> {
-            if (isFrontResearcher) {
-                binding.researcherFront.setVisibility(View.INVISIBLE);
-                binding.researcherBack.setVisibility(View.VISIBLE);
-                YoYo.with(Techniques.FadeIn).duration(300).playOn(binding.researcherBack);
-                isFrontResearcher = false;
-            } else {
-                binding.researcherFront.setVisibility(View.VISIBLE);
-                binding.researcherBack.setVisibility(View.INVISIBLE);
-
-                YoYo.with(Techniques.FadeIn).duration(300).playOn(binding.researcherFront);
-                isFrontResearcher = true;
-            }
-            return true;
-        });
-
     }
 }
